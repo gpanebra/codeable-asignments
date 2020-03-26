46fa9460f5768aa6a909d077fc750c71fdd6c702
gpanebra <gpanebramoina@gmail.com>
2020-03-26 11:34:57 -0500

    Added styled table + credit (incomplete)

---
+++ Module1/Week2/Day3/cash-credit/credit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    long num;
+    do
+    {
+        num = get_long("Number: ");
+        while ()
+        {
+            num
+        }
+        if ()
+        {
+        }
+        else
+        {
+            printf("INVALID");
+        }
+    } while (true);
+}
